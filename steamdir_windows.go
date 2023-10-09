26c5bf829c8ed462e072d88b20e862842456df90
ddeityy <ddeityy@gmail.com>
2023-10-09 15:52:33 +0300

    windows support

--- steamdir_windows.go
+++ steamdir_windows.go
@@ -4,20 +4,34 @@
 package steamlocate
 
 import (
-	"fmt"
 	"log"
+	"runtime"
+
+	"golang.org/x/sys/windows/registry"
 )
 
-func (s *SteamDir) Locate() {
-	k, err := registry.OpenKey(registry.LOCAL_MACHINE, "SOFTWARE\Microsoft\Windows NT\CurrentVersion", registry.QUERY_VALUE)
+func (s *SteamDir) locate() {
+	var k registry.Key
+	var err error
+
+	switch runtime.GOARCH {
+	case "386":
+		k, err = registry.OpenKey(registry.LOCAL_MACHINE, `SOFTWARE\Wow6432Node\Valve\Steam`, registry.QUERY_VALUE)
+	case "amd64":
+		k, err = registry.OpenKey(registry.LOCAL_MACHINE, `SOFTWARE\Valve\Steam`, registry.QUERY_VALUE)
+	default:
+		log.Fatalf("Failed to locate steam.")
+	}
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to locate steam.")
 	}
 	defer k.Close()
 
-	s, _, err := k.GetStringValue("SystemRoot")
+	steamPath, _, err := k.GetStringValue("InstallPath")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Windows system root is %q\n", s)
+	s.Path = steamPath
+	s.LibraryFolders.discover(steamPath)
+	s.SteamApps.discover()
 }
